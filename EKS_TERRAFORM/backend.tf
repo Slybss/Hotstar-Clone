38636fcc860869be8a65167e9456c3f7e1d4c223
Sylvester Bassey <115729068+Slybss@users.noreply.github.com>
2024-07-02 10:24:08 +0100

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "slybss-mybucket217" # Replace with your actual S3 bucket name
+    bucket = "slybss-mybucket218" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
